b09c3ed5d5db7f62d092b7be10de126dedd89820
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-03 16:12:18 +1200

    fix type error

--- pages/components/SingleProject.tsx
+++ pages/components/SingleProject.tsx
@@ -12,7 +12,7 @@ function SingleProject({ project }: Props) {
       <section className="flex flex-col text-left p-10 bg-slate-600 text-zinc-100 m-6 rounded-xl">
         <h3>{project.title}</h3>
         {project.role && <h2 className="font-medium">{project.role}</h2>}
-        {project.description.map((par) => (
+        {project.description.map((par: string) => (
           <p key={project.title + par}>{par}</p>
         ))}
       </section>
