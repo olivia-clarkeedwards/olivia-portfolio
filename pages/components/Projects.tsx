a053b925b7839d9cd54fe8b6673f2f117253e4c1
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-03 18:34:00 +1200

    update project and projects to use useState and useEffect

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -1,15 +1,22 @@
 import SingleProject from './SingleProject'
 
 import { Project } from './../../common/models'
+import { useEffect, useState } from 'react'
 
 interface Props {
   projects: Project[]
 }
 
 function Projects({ projects }: Props) {
+  const [allProjects, setAllProjects] = useState([] as Project[])
+
+  useEffect(() => {
+    setAllProjects(projects)
+  }, [projects])
+
   return (
     <>
-      {projects.map((project: Project) => (
+      {allProjects.map((project: Project) => (
         <SingleProject project={project} key={project.date + project.title} />
       ))}
     </>
